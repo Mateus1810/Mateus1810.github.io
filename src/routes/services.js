215bf11859d6757ed55c3d67b6e35476a0da3a13
Mateus1810 <neuzapazeli@gmail.com>
2025-01-22 12:06:09 -0300

    teste servico 1

---
+++ src/routes/services.js
@@ -0,0 +1,22 @@
+// src/routes/services.js (ou o caminho que você preferir)
+
+const services = [
+  {
+    href: "pergunta_manutencao.html?servico=manutenção%20de%20computadores",
+    img: "../src/img/Design sem nome.png",
+    alt: "Ícone Manutenção de Computadores",
+    label: "Manutenção de Computadores",
+  },
+  {
+    href: "pergunta_intencao.html",
+    img: "img/pc-de-montagem-hardware-de-computador-pessoal_169241-849.jpg",
+    alt: "Ícone Montagem e Upgrade de Computadores",
+    label: "Montagem e Upgrade de Computadores",
+  },
+  {
+    href: "pergunt.html?servico=design%20grafico", // Corrigido para 'pergunta.html'
+    img: "img/em-breve (1).png",
+    alt: "Ícone Design Gráfico",
+    label: "EM BREVE",
+  },
+];
