3aba2ab5af0a5dca577989b3fe9a58621f55aae3
Mateus1810 <neuzapazeli@gmail.com>
2025-01-23 11:31:17 -0300

    9

--- src/controller/formClienteMontagem.js
+++ src/controller/formClienteMontagem.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const urlParams = new URLSearchParams(window.location.search);
         const servico = urlParams.get('servico');
         const problema = urlParams.get('problema') || '';
-        const message = `Olá, meu nome é ${clientName}. Gostaria de mais informações sobre o serviço de ${servico}. O que eu quero dar upgrade é no(a) ${problema}.`;
+        const message = `Olá, meu nome é ${clientName}. Gostaria de mais informações sobre o serviço de ${servico}. Problema: ${problema}.`;
         const whatsappUrl = `https://wa.me/5533984024108?text=${encodeURIComponent(message)}`;
         window.location.href = whatsappUrl;
     });
